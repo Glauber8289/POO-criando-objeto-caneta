96e65e9dd183771dc55c2e573468ab89d6a38abc
Glauber Peterson <Petersonglauber@hotmail.com>
2022-08-08 23:58:30 -0300

    Aplicando conceitos POO

---
+++ src/aula/pkg2/AULA2.java
@@ -0,0 +1,14 @@
+
+package aula.pkg2;
+
+public class AULA2 {
+
+    public static void main(String[] args) {
+       Caneta c1 = new Caneta ();
+       c1.cor="azul";
+       c1.ponta = 0.5f;
+       c1.tampada=false;
+       c1.status();
+    }
+    
+}
---
+++ src/aula/pkg2/Caneta.java
@@ -0,0 +1,36 @@
+
+package aula.pkg2;
+
+
+public class Caneta {
+    String modelo;
+     String cor;  
+     Float ponta;
+     int carga;
+     boolean tampada;
+     void status () {
+         System.out.println("Uma caneta "  +  this.cor);
+         System.out.println("Esta tampada ?"  +  this.tampada);
+         System.out.println("Qual e o modelo ?"  +  this.modelo);
+         System.out.println("Qual a ponta ?"  +  this.ponta);
+         System.out.println("Qual e a carga ?"  +  this.carga);
+         
+     }
+     
+     void rabiscar (){
+     
+         
+         
+     }
+         
+         void tampar (){
+         
+         
+         
+     }
+
+     void destampar () {
+         
+         
+     }
+}
